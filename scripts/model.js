9a516e3a4f5e6f76ea75ac30a5ccd358670fa1f5
Unknown <lewistran96@gmail.com>
2018-07-24 16:43:38 +0100

    Added interactive highlight and circlip feature

--- scripts/model.js
+++ scripts/model.js
@@ -1,17 +1,3 @@
-class Bearing {
-  // Renders bearing (inner race, outer race and ball) based on x, y coordinates
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-
-    this.render = function() {
-      rect(x, y - 12, 30, 12)
-      rect(x, y + 12, 30, 12)
-      ellipse(x, y, 18, 18)
-    }
-  }
-}
-
 class Housing {
   // Renders bearing housing based on left and right constraints, and x coordinate
   constructor(x, left, right) {
@@ -20,12 +6,15 @@ class Housing {
     this.right = right;
 
     this.render = function() {
-      fill(240, 240, 240)
-        rect(x, centre.horizontal, 60, 180)
-      fill(200, 200, 200)
-        rect(x, offset.bottom - 30, 60, 24)
-        rect(x, offset.top + 30, 60, 24)
-      fill(255, 255, 255)
+      fill(240, 240, 240);
+        rect(x, centre.horizontal, 60, 180);
+      fill(200, 200, 200);
+        rect(x, offset.bottom - 30, 60, 24);
+        rect(x, offset.top + 30, 60, 24);
+      fill('white');
+
+      ballAndRace(x, offset.top);
+      ballAndRace(x, offset.bottom);
     }
   }
 }
@@ -39,22 +28,44 @@ class Shaft {
     this.rightright = rightright;
 
     this.render = function() {
-      rect(centre.vertical, centre.horizontal, centre.length, 60)
-      centreline(centre.horizontal, centre.length)
+      rect(centre.vertical, centre.horizontal, centre.length, 60);
+      centreline(centre.horizontal, centre.length);
     }
   }
 }
 
 class Circlip {
   // Renders circlip (internal or external) based on x coordinate and position
-  constructor(x, type) {
+  constructor(x, fit) {
     this.x = x;
-    this.type = type;
+    this.fit = fit;
 
     this.render = function() {
-      fill(100, 100, 255)
-      rect(x, centre.horizontal + type, 6, 12)
-      rect(x, centre.horizontal - type, 6, 12)
+      fill(100, 100, 255);
+        rect(x, centre.horizontal + fit, 6, 12);
+        rect(x, centre.horizontal - fit, 6, 12);
+    }
+
+    this.highlight = function() {
+      stroke('red');
+      fill(255, 0, 0, 20);
+        rect(x, centre.horizontal + fit, boxsize, boxsize, 5, 5, 5, 5);
+        rect(x, centre.horizontal - fit, boxsize, boxsize, 5, 5, 5, 5);
+      stroke('black');
+    }
+
+    this.checkOver = function() {
+      if (mouseIsPressed) {
+        if (mouseOver(this.x, this.fit)) {
+          model.run = model.run.concat(this);
+        }
+        else {
+          highlightCount++;
+          if (highlightCount > model.runHighlight.length) {
+            resetHighlight();
+          }
+        }
+      }
     }
   }
 }
@@ -62,15 +73,15 @@ class Circlip {
 function setup() {
   // Canvas Setup
   canvas = {
-    width: 0.95 * document.getElementById('model').offsetWidth,
+    width: 0.95 * document.getElementById('model.ID').offsetWidth,
     height: 400,
   }
-  canvas.dimensions = createCanvas(canvas.width, canvas.height),
-  canvas.dimensions.parent('model')
+  canvas.dimensions = createCanvas(canvas.width, canvas.height);
+  canvas.dimensions.parent('model.ID');
 
   // Position coordinates
-  rectMode(CENTER)
-  ellipseMode(CENTER)
+  rectMode(CENTER);
+  ellipseMode(CENTER);
   offset = {
     top: canvas.height * 0.62,
     bottom: canvas.height * 0.38,
@@ -88,17 +99,13 @@ function setup() {
     internal: 30,
     external: 65,
   }
+  boxsize = 20;
+  highlightCount = 0; /* Global included to prevent highlight from resetting before completion */
 
   // Models initialisation
   model.housing = [
     new Housing(offset.left, false, false),
     new Housing(offset.right, false, false),
-  ]
-  model.bearing = [
-    new Bearing(offset.left, offset.top),
-    new Bearing(offset.right, offset.top),
-    new Bearing(offset.left, offset.bottom),
-    new Bearing(offset.right, offset.bottom),
   ]
   model.shaft = [
     new Shaft(false, false, false, false),
@@ -106,17 +113,17 @@ function setup() {
   model.circlip = [
     new Circlip(offset.left + shift.left, shift.internal),
     new Circlip(offset.left + shift.right, shift.internal),
-    new Circlip(offset.left + shift.left, shift.external),
-    new Circlip(offset.left + shift.right, shift.external),
     new Circlip(offset.right + shift.left, shift.internal),
     new Circlip(offset.right + shift.right, shift.internal),
+    new Circlip(offset.left + shift.left, shift.external),
+    new Circlip(offset.left + shift.right, shift.external),
     new Circlip(offset.right + shift.left, shift.external),
     new Circlip(offset.right + shift.right, shift.external),
   ]
 
   // Set default model
-  model.reset = model.housing.slice(0,2).concat(model.bearing.slice(0,4)).concat(model.shaft.slice(0,1));
-  model.run = model.reset;
+  model.reset = model.housing.slice(0,2).concat(model.shaft.slice(0,1));
+  resetDraw();
 }
 
 function draw() {
@@ -125,6 +132,54 @@ function draw() {
 
   for (let i=0; i<model.run.length; i++) {
     model.run[i].render();
+  }
+
+  for (let i=0; i<model.runHighlight.length; i++){
+    model.runHighlight[i].highlight();
+    model.runHighlight[i].checkOver();
+  }
+}
+
+function resetDraw() {
+  // Resets model render
+  model.run = model.reset;
+  model.runHighlight = [];
+}
+
+function resetHighlight() {
+  // Resets highlight render
+  highlightCount = 0;
+  model.runHighlight = [];
+}
+
+function feature(constraint) {
+  // Assigns constraint
+  model.runHighlight = [];
+  switch (constraint) {
+    case 'intCirclip':
+      model.runHighlight = model.circlip.slice(0,4);
+    break
+    case 'extCirclip':
+      model.runHighlight = model.circlip.slice(4,8);
+    break
+  }
+}
+
+function mouseOver(x, fit) {
+  // Returns true if mouse is over given box size
+  if (mouseX > x - boxsize/2 && mouseX < x + boxsize/2 &&
+      mouseY > centre.horizontal + fit - boxsize/2 && mouseY < centre.horizontal + fit + boxsize/2) {
+    console.log('Yo1')
+    return true;
+  }
+  else if (mouseX > x - boxsize/2 && mouseX < x + boxsize/2 &&
+           mouseY > centre.horizontal - fit - boxsize/2 && mouseY < centre.horizontal - fit + boxsize/2) {
+    console.log('Yo2')
+    return true;
+  }
+  else {
+    console.log('Fail')
+    return false;
   }
 }
 
@@ -137,4 +192,11 @@ function centreline(y, length) {
       prev += 20;
     }
     line(prev + 4, y, prev + 16, y)
+}
+
+function ballAndRace(x, y) {
+  // Draws the bearing ball and race at a given x and y location
+      rect(x, y - 12, 30, 12)
+      rect(x, y + 12, 30, 12)
+      ellipse(x, y, 18, 18)
 }
